e1b124b9e6a8f31267f9d0683bf4a2b2d820df00
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-09-21 03:34:29 +0000

    new include file

---
+++ read_attribute_code.f90
@@ -0,0 +1,21 @@
+    ! read attribute 'attname' return in 'values'.  If optional
+    ! argument 'varname' is given, an variable attribute is returned.
+    ! if the attribute is an 1d array, values should be an allocatable 1d
+    ! array of the correct type. if values is allocated, it be deallocated
+    ! and reallocated.
+    type(Dataset), intent(in) :: dset
+    character(len=*), intent(in), optional :: varname
+    character(len=*), intent(in) :: attname
+    integer ncerr, varid, nvar, nlen
+    if(present(varname))then
+        nvar = nvar_(dset,varname)
+        varid = dset%variables(nvar)%varid
+    else
+        varid = NF90_GLOBAL
+    endif 
+    ncerr = nf90_inquire_attribute(dset%ncid, varid, attname, len=nlen)
+    call nccheck(ncerr)
+    if (allocated(values)) deallocate(values)
+    allocate(values(nlen))
+    ncerr = nf90_get_att(dset%ncid, varid, trim(attname), values)
+    call nccheck(ncerr)
