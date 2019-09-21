7addc790e74ae80e34de46b75eb1a1ebefafda0a
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-09-21 03:35:06 +0000

    new include files

---
+++ read_vardata_code_1d.f90
@@ -0,0 +1,17 @@
+    ! read all data from variable varname, return in it array values.
+    ! on input, values should be an allocatable array with the correct
+    ! with the correct number of dimensions.  If values is allocated,
+    ! it we deallocated and reallocated.
+    type(Dataset), intent(in) :: dset
+    character(len=*), intent(in) :: varname
+    integer ncerr, nvar, n1, ndim
+    nvar = nvar_(dset,varname)
+    if (dset%variables(nvar)%ndims /= 1) then
+       print *,'rank of data array does not match number of variable dims'
+       stop "stopped"
+    endif
+    n1 = dset%variables(nvar)%dimlens(1)
+    if (allocated(values)) deallocate(values)
+    allocate(values(n1))
+    ncerr = nf90_get_var(dset%ncid, dset%variables(nvar)%varid, values)
+    call nccheck(ncerr)
---
+++ read_vardata_code_4d.f90
@@ -0,0 +1,20 @@
+    ! read all data from variable varname, return in it array values.
+    ! on input, values should be an allocatable array with the correct
+    ! with the correct number of dimensions.  If values is allocated,
+    ! it we deallocated and reallocated.
+    type(Dataset), intent(in) :: dset
+    character(len=*), intent(in) :: varname
+    integer ncerr, nvar, n1,n2,n3,n4, ndim
+    nvar = nvar_(dset,varname)
+    if (dset%variables(nvar)%ndims /= 4) then
+       print *,'rank of data array does not match number of variable dims'
+       stop "stopped"
+    endif
+    n1 = dset%variables(nvar)%dimlens(1)
+    n2 = dset%variables(nvar)%dimlens(2)
+    n3 = dset%variables(nvar)%dimlens(3)
+    n4 = dset%variables(nvar)%dimlens(4)
+    if (allocated(values)) deallocate(values)
+    allocate(values(n1,n2,n3,n4))
+    ncerr = nf90_get_var(dset%ncid, dset%variables(nvar)%varid, values)
+    call nccheck(ncerr)
